c9a8a717ab9a716d0816cfc05352b0d6fd648ba8
Armie Margaret Lee <armiemargaretlee@yahoo.com>
2014-08-20 17:53:53 -0400

    Must be logged in to review books [finishes #77332262]

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:show, :index]
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   # GET /reviews
