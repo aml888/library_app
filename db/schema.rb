b2364f664b422f0e384049c1133aaa08e29b6c2e
Armie Margaret Lee <armiemargaretlee@yahoo.com>
2014-08-22 15:05:49 -0400

    Create relationship model [finishes #77468520]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822162008) do
+ActiveRecord::Schema.define(version: 20140822184747) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -39,6 +39,17 @@ ActiveRecord::Schema.define(version: 20140822162008) do
   end
 
   add_index "books", ["user_id"], name: "index_books_on_user_id"
+
+  create_table "relationships", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "reviews", force: true do |t|
     t.string   "user_name"
