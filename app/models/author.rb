f35cec6846d9b8c98f8bde50f2c869a374012c72
Armie Margaret Lee <armiemargaretlee@yahoo.com>
2014-08-30 10:15:40 -0400

    Create show page for newly added book [finishes #77925232]

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
 	has_many :authorships
 	has_many :books, through: :authorships
+	belongs_to :book
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,14 +4,14 @@ class Book < ActiveRecord::Base
 	has_many :authors, through: :authorships
 	mount_uploader :picture, PictureUploader
 	has_many :reviews
-	accepts_nested_attributes_for :authors
+	accepts_nested_attributes_for :authors, reject_if: proc { |attributes| attributes['name'].blank? }
 	has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
 	has_many :followers, through: :reverse_relationships, source: :follower
     acts_as_taggable
 	scope :approved, -> { where(approved: true) }
 	scope :pending_approval, -> { where(approved: [false, nil]) }
 
-	validates :title, :ISBN, :picture, presence: {message: 'must not be blank'}
+	validates :title, :ISBN, presence: {message: 'must not be blank'}
 	
 	
 	def self.search(search)
